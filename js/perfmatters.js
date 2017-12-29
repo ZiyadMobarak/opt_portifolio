5f41e51b6519417c5413b40b4f20837a0f8cc1a7
ZiyadMobarak <ZiyadMobarak@hotmail.com>
2017-12-29 08:56:45 +0300

    Original Status
    Original Status

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -1,3 +1,6 @@
+// Measuring the Critical Rendering Path with Navigation Timing
+// https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
+
 function logCRP() {
   var t = window.performance.timing,
     dcl = t.domContentLoadedEventStart - t.domLoading,
@@ -6,16 +9,6 @@ function logCRP() {
   stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
 }
 
-function ga(w, g) {
-  w['GoogleAnalyticsObject'] = g;
-  w[g] = w[g] || function() {
-    (w[g].q = w[g].q || []).push(arguments)
-  };
-  w[g].l = 1 * new Date();
-}
-
 window.addEventListener("load", function(event) {
   logCRP();
-  ga('create', 'UA-XXXX-Y');
-  ga('send', 'pageview');
 });
