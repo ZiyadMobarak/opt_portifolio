68c8ae539eb702451ea8e86fcaa5e67009217a47
ZiyadMobarak <ZiyadMobarak@hotmail.com>
2017-12-29 08:08:32 +0300

    Merge JS
    Merge JS

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -1,6 +1,3 @@
-// Measuring the Critical Rendering Path with Navigation Timing
-// https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
-
 function logCRP() {
   var t = window.performance.timing,
     dcl = t.domContentLoadedEventStart - t.domLoading,
@@ -9,6 +6,16 @@ function logCRP() {
   stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
 }
 
+function ga(w, g) {
+  w['GoogleAnalyticsObject'] = g;
+  w[g] = w[g] || function() {
+    (w[g].q = w[g].q || []).push(arguments)
+  };
+  w[g].l = 1 * new Date();
+}
+
 window.addEventListener("load", function(event) {
   logCRP();
+  ga('create', 'UA-XXXX-Y');
+  ga('send', 'pageview');
 });
